72e7824ac3e90630fdbe79abb25e58057d970a5a
brandongossen <brandongossen@yahoo.com>
2017-09-19 14:48:57 -0500

    ControlFlowExercise.java Second commit
    -Refactor all while and do-while loops to a for loopa

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,40 +1,40 @@
 import java.util.Scanner;
 
 public class ControlFlowExercises {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner loops = new Scanner(System.in);
 
 //       Problem 1a.
-        int i = 5;
-        while(i <= 15) {
+//        Refactor to For Loop
+        for (int i = 5; i <= 15; i++) {
             System.out.print(i + " ");
-            i++;
         }
 
-//        Problem 1b.
-//        System.out.println();
-//
-//        int j = 0;
-//        do {
-//            System.out.println(j);
-//            j += 2;
-//
-//        } while (j <= 100);
-//    counts by 2 from 0 to 100
 
-//        System.out.println();
-//        int j = 100;
-//        do {
-//            System.out.println(j);
-//            j -= 5;
-//        } while (j >= -10);
+//        Problem 1b.
+//        Refactor to For Loop
+        System.out.println();
+
+        for (int j = 0; j <= 100; j += 2) {
+            System.out.println(j);
+        }
+//        counts by 2 from 0 to 100
+//
+//        Refactor to For Loop
+        System.out.println();
+
+        for (int j = 100; j >= -10; j -= 5) {
+            System.out.println(j);
+        }
 //    counts down by -5 from 100 to -10
 
+
+//        Refactor to For Loop
         System.out.println();
-        long j = 2;
-        do {
+
+        for (long j = 2; j < 1000000; j *= j) {
             System.out.println(j);
-            j *= j;
-        } while (j < 100000);
+        }
+//    squares each number until it reached 1000000
     }
 }
