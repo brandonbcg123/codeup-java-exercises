661207aa11bed11859377c1ba2ecfb9f14d6977d
brandongossen <brandongossen@yahoo.com>
2017-09-19 15:07:57 -0500

    ControlFlowExercise.java Third commit
    -Complete problem 2
    
    -use for loop and if/else statement to produce "FizzBuzz" for multiples of 5 and 3, "Fizz" for multiples of 3, and "Buzz" for multiples of 5

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,37 +4,52 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner loops = new Scanner(System.in);
 
-//       Problem 1a.
-//        Refactor to For Loop
-        for (int i = 5; i <= 15; i++) {
-            System.out.print(i + " ");
-        }
-
-
-//        Problem 1b.
-//        Refactor to For Loop
-        System.out.println();
-
-        for (int j = 0; j <= 100; j += 2) {
-            System.out.println(j);
-        }
-//        counts by 2 from 0 to 100
+////       Problem 1a.
+////        Refactor to For Loop
+//        for (int i = 5; i <= 15; i++) {
+//            System.out.print(i + " ");
+//        }
 //
-//        Refactor to For Loop
-        System.out.println();
+//
+////        Problem 1b.
+////        Refactor to For Loop
+//        System.out.println();
+//
+//        for (int j = 0; j <= 100; j += 2) {
+//            System.out.println(j);
+//        }
+////        counts by 2 from 0 to 100
+////
+////        Refactor to For Loop
+//        System.out.println();
+//
+//        for (int j = 100; j >= -10; j -= 5) {
+//            System.out.println(j);
+//        }
+////    counts down by -5 from 100 to -10
+//
+//
+////        Refactor to For Loop
+//        System.out.println();
+//
+//        for (long j = 2; j < 1000000; j *= j) {
+//            System.out.println(j);
+//        }
+////    squares each number until it reached 1000000
+//    }
+//}
 
-        for (int j = 100; j >= -10; j -= 5) {
-            System.out.println(j);
+//  Problem 2.
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
-//    counts down by -5 from 100 to -10
-
-
-//        Refactor to For Loop
-        System.out.println();
-
-        for (long j = 2; j < 1000000; j *= j) {
-            System.out.println(j);
-        }
-//    squares each number until it reached 1000000
     }
 }
