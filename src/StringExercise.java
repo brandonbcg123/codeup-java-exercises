16f9831fd8cd77273ea693630284e3920207f8a4
brandongossen <brandongossen@yahoo.com>
2017-09-20 13:03:13 -0500

    StringExercise.java first commit
    -complete exercise 1 (String basics)
    
    -create a string variable named "message" and print all the desired outputs using the same variable and only print one statement that shows all the outputs

---
+++ src/StringExercise.java
@@ -0,0 +1,16 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String message;
+
+        message = "We don't need no education\nWe don't need no thought control\n";
+
+        message += "\nCheck \"this\" out!, \"s inside of \"s!\n";
+
+        message += "\nIn windows, the main drive is usually C:\\\n";
+
+        message += "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+
+        System.out.println(message);
+    }
+}
