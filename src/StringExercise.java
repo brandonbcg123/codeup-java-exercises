1b6a562776b59e3f983f3af7117b19bf844b8b7d
brandongossen <brandongossen@yahoo.com>
2017-09-21 09:21:49 -0500

    StringExercise Final Copy
    -Print out each message after each output
    
    -comment this version out and whole have part 2 running

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,21 +1,35 @@
 //Problem 1
 public class StringExercise {
     public static void main(String[] args) {
-
+//    Part 1
 //        String message;
 //
 //        message = "We don't need no education\nWe don't need no thought control\n";
-//
+//        System.out.println(message);
 //        message += "\nCheck \"this\" out!, \"s inside of \"s!\n";
-//
+//        System.out.println(message);
 //        message += "\nIn windows, the main drive is usually C:\\\n";
-//
+//        System.out.println(message);
 //        message += "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
-//
 //        System.out.println(message);
 
-        String message = "We don't need no education\nWe don't need no thought control\n\nCheck \"this\" out!, \"s inside of \"s!\n\nIn windows, the main drive is usually C:\\\nI can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+//  Part 2: One way to do it
+//        String message = "We don't need no education\nWe don't need no thought control\n\nCheck \"this\" out!, \"s inside of \"s!\n\nIn windows, the main drive is usually C:\\\nI can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+
+//        System.out.println(message);
+
+//        Part 2 : Another way to do it
+        String message;
+
+        message = "We don't need no education\nWe don't need no thought control\n";
+
+        message += "\nCheck \"this\" out!, \"s inside of \"s!\n";
+
+        message += "\nIn windows, the main drive is usually C:\\\n";
+
+        message += "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
 
         System.out.println(message);
+
     }
 }
