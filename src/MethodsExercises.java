6a6ee45a64eb8820f17179f90b5c86a1fec90390
brandongossen <brandongossen@yahoo.com>
2017-09-21 12:26:36 -0500

    MethodsExercises Second Commit
    -create a method that validates that user input is in a certain range using a scanner with if/else statements
    
    -prompt the user to input a new input if he or she entered an invalid input

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,13 +1,16 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
-    public static void main(String[] args){
-        System.out.println(add(4,6));
+    public static void main(String[] args) {
+        System.out.println(add(4, 6));
         System.out.println(subtract(4, 6));
         System.out.println(multiply(4, 6));
         System.out.println(divide(4, 6));
         System.out.println(module(4, 6));
+        int userNumber = getInteger(4, 11);
     }
 
-//        Problem 1
+    //        Problem 1
     public static int add(int a, int b) {
         return a + b;
     }
@@ -29,4 +32,16 @@ public class MethodsExercises {
         return a % b;
     }
 
+    //    Problem 2
+    public static int getInteger(int min, int max) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number between " + min + " and " + max);
+        int userInput = scan.nextInt();
+        if (userInput < min || userInput > max) {
+            System.out.println("This is an invalid input, please try again.");
+            return getInteger(min, max);
+        } else {
+            return userInput;
+        }
+    }
 }
