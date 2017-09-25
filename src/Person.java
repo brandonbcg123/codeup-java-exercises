30cddb3c0d04f86e771e2498deefc114e81daaed
brandongossen <brandongossen@yahoo.com>
2017-09-25 13:47:11 -0500

    Person.java exercise 2nd commit
    -test out code blocks to see if they would print out what I expected.

--- src/Person.java
+++ src/Person.java
@@ -4,6 +4,10 @@ public class Person {
 
     private String name;
 
+    public Person (String name) {
+        this.name = name;
+    }
+
     public String getName(){
         return this.name;
         //getter makes sure its readable
@@ -20,12 +24,46 @@ public class Person {
 
 
     public static void main(String[] args) {
-        Person person = new Person();
-        person.setName("T-Payne");
+        Person person = new Person("T-Payne");
+//        person.setName("T-Payne");
         person.sayHello();
         System.out.println(person.getName());
         person.setName("Brandon");
         person.sayHello();
         System.out.println(person.getName());
+
+//Problem 2
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+
+        //Since this is not a static class the following codes should not produce John
+
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+        //The first print out will result in true because the .equals method is applied to both matching them as sring values, while the second print out will result in false because strings are not primitive data types so these these two would be compared in what time it took in memory.
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        //This will print out true because person2 was made directly equal to person1 as an object value in the same memory in time instead of looking it as a primitive value.
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+
+//        The first two will print the name John because person2 was made          directly equal to person1 as an object value in the same memory          in time.
+
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+
+        //These two both printed out Jane when I initially thought only person2 would print out Jane and person1 would print out John, but with both objects pointed at the same memory one will change to what the other one will change to.
+//
     }
+
+
 }
