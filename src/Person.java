c35fa5fa35c5aaa7a8d65486229bce3253154a67
brandongossen <brandongossen@yahoo.com>
2017-09-25 12:31:19 -0500

    Person.java exercise 1st commit
    -create a private name property and give it a string type and place it within the Person class.
    
    -create a getter, setter, and a method called sayHello
    
    -create a main method that creates a new instance of the Person object
    
    -pass the methods into the main method.

---
+++ src/Person.java
@@ -0,0 +1,31 @@
+//Problem 1
+
+public class Person {
+
+    private String name;
+
+    public String getName(){
+        return this.name;
+        //getter makes sure its readeable
+    }
+
+    public void setName (String name){
+        this.name = name;
+        //setter make sure its writable / allowing name to be changed
+    };
+
+    public void sayHello() {
+        System.out.println(this.name);
+    }
+
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("T-Payne");
+        person.sayHello();
+        System.out.println(person.getName());
+        person.setName("Brandon");
+        person.sayHello();
+        System.out.println(person.getName());
+    }
+}
