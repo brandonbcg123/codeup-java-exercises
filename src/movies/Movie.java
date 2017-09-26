810597cab3ad500499d6ef2e837c0418698e75aa
brandongossen <brandongossen@yahoo.com>
2017-09-26 16:06:41 -0500

    Movie List app 1st commit
    -Create a class for Movie, MoviesApplication, and MoviesArray.
    
    -Create a new instance of input in the MoviesApplication class main method.

---
+++ src/movies/Movie.java
@@ -0,0 +1,10 @@
+package movies;
+
+public class Movie {
+    private String name;
+    private String category;
+
+    public Movie (String name, String category) {
+
+    }
+}
---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,14 @@
+package movies;
+
+
+import util.Input;
+
+
+public class MoviesApplication {
+    public static void main(String[] args) {
+
+        Input input = new Input();
+
+        }
+
+}
