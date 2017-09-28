b0329b0e1e8613e66e6e8e95c06043473459d0da
brandongossen <brandongossen@yahoo.com>
2017-09-28 16:35:35 -0500

    Interfaces and Abstract Exercise 1st Commit
    -Create an interface  with methods to getPerimeter and getArea
    
    _Create an abstract class called Quadrilateral that has protected properties length and width
    
    -create a constructor to accept two number for length and width
    
    -set get methods for the length and width
    
    -form an abstract set method for length and width

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,16 +1,16 @@
 package shapes;
 
 public class Square extends Rectangle {
-    public Square (double side) {
-    super (side, side);
-    }
-
-    public double getPerimeter () {
-        return this.length * 4;
-
-    }
-
-    public double getArea () {
-        return this.length * this.length;
-    }
 }
+//    public Square (double side) {
+//    super (side, side);
+//    }
+//
+//    public double getPerimeter () {
+//        return this.length * 4;
+//
+//    }
+//
+//    public double getArea () {
+//        return this.length * this.length;
+//    }
