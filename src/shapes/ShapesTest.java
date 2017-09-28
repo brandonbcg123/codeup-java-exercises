01181dff09affa3e26819653640d012f359c4f89
brandongossen <brandongossen@yahoo.com>
2017-09-28 12:36:07 -0500

    Inheritance and Polymorphism 6th Commit Bonus Answered
    -Create a static method named showRectangleInfo in ShapesTest.java with a type of Rectangle
    
    -Have this method pass a rectangle's area and perimeter and then have it pass a square's arear and perimeter

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -8,6 +8,23 @@ public class ShapesTest {
         Rectangle box2 = new Square(6);
         System.out.println("The perimeter of the square is: " + box2.getPerimeter());
         System.out.println("The area of the square is: " + box2.getArea());
+        showRectangleInfo(box1);
+        showRectangleInfo(box2);
        }
 
+    public static void showRectangleInfo (Rectangle rectangle) {
+        System.out.println(rectangle.getPerimeter());
+        System.out.println(rectangle.getArea());
     }
+
+    }
+//    ** Bonus **
+//
+//            1. Inside of `ShapesTest`, create a static method named `showRectangleInfo`
+//        that accepts an object of type `Rectangle` and displays the rectangle's
+//        area and perimeter.
+//
+//
+// answer:
+// Can you pass an object of type `Square` to this method?
+//yes because it is inherited from the rectangle class because I made the class square extended from the rectangle class.
