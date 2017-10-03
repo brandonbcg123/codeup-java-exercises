3597cb24d59993172d68c07cca752436f73b2b81
brandongossen <brandongossen@yahoo.com>
2017-10-03 14:52:22 -0500

    Exceptions and Error Handling Exercise 1st Commit
    -Turn my scanner classes into a string
    
    -Add a "try" that returns a .valueOf method for Integer and Double to automatically parse the userinput into a numerical value
    
    -Add a "catch" that will use a NumberFormatException if the given input cannot be parsed as an integer or double.

--- src/util/Input.java
+++ src/util/Input.java
@@ -2,6 +2,8 @@ package util;
 
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 //Problem 3
 public class Input {
 
@@ -19,7 +21,7 @@ public class Input {
 
     public Boolean yesNo(String prompt) {
         System.out.println(prompt);
-        String userInput = scanner.next();
+        String userInput = scanner.nextLine();
         if (userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes")) {
             return true;
         } else {
@@ -29,25 +31,34 @@ public class Input {
 
     public int getInt(String prompt, int min, int max) {
         System.out.println(prompt);
-        int userInput = scanner.nextInt();
+        int userInput = getInt();
         if (userInput < min || userInput > max) {
             System.out.println("This is an invalid input, please try again.");
             return getInt(prompt, min, max);
         } else {
-            scanner.nextLine();
             return userInput;
         }
     }
 
-    public int getInt(String prompt){
+    public int getInt(){
+        String userInput = scanner.nextLine();
+        try {
+            return Integer.valueOf(userInput);
+        } catch (NumberFormatException nfe) {
+            System.out.println("Sorry, you did not enter an integer. Please enter an integer.");
+            return getInt();
+        }
+
+    }
+
+    public int getInt(String prompt) {
         System.out.println(prompt);
-        int userInput = scanner.nextInt();
-        return userInput;
+        return getInt();
     }
 
     public double getDouble(String prompt, double min, double max) {
         System.out.println(prompt);
-        double userInput = scanner.nextDouble();
+        double userInput = getDouble();
         if (userInput < min || userInput > max) {
             System.out.println("This is an invalid input, please try again.");
             return getDouble(prompt, min, max);
@@ -56,9 +67,19 @@ public class Input {
         }
     }
 
+    public double getDouble() {
+        String userInput = scanner.nextLine();
+        try {
+            return Double.valueOf(userInput);
+        } catch (NumberFormatException nfe) {
+            System.out.println("Sorry, you did not enter a number or one within the correct range. Please try again.");
+            return getDouble();
+        }
+
+    }
+
     public double getDouble(String prompt){
         System.out.println(prompt);
-        double userInput = scanner.nextDouble();
-        return userInput;
+        return getDouble();
     }
 }
